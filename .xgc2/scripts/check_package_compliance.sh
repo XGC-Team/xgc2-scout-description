b434c1b2ad9cd6d5e0829ef139ce97161508f301
Lxk_buaa <867768510@qq.com>
2026-07-10 04:44:05 +0800

    Set Scout simulated IMU to 200 Hz

--- .xgc2/scripts/check_package_compliance.sh
+++ .xgc2/scripts/check_package_compliance.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 grep -q '^id: xgc2-scout-description$' .xgc2/product.yml
-grep -q '^version: 0.4.10-3$' .xgc2/product.yml
+grep -q '^version: 0.4.10-4$' .xgc2/product.yml
 grep -q '<name>scout_description</name>' package.xml
 grep -q 'ros-noetic-lms1xx' .xgc2/product.yml
 grep -q 'ros-noetic-xacro' .xgc2/product.yml
